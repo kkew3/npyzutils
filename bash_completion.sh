a087e3f3da71c93b2c9bd0b31ed25f6298496710
kkew3 <kps6326@hotmail.com>
2019-04-09 17:05:29 -0700

    Initial commit of bash_completion script for the launch scripts (not `src/*.py` themselves!)

---
+++ bash_completion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+complete -f -X '!*.npy' npycat
+complete -f -X '!*.np[yz]' npyz2img
+complete -f -X '!*.np[yz]' npyzshape
+complete -f -X '!*.np[yz]' npyzz
+complete -f -X '!*.npz' npz2npy
+complete -f -X '!*.npz' npzcat
